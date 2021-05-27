2e27526279dc54b8faeff7f077ba0bdc4d8b4ffb
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-05-27 17:49:14 -0500

    added bloc provider

--- flutter/feature/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/presentation/{{#snakeCase}}{{name}}{{/snakeCase}}_screen.dart
+++ flutter/feature/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/presentation/{{#snakeCase}}{{name}}{{/snakeCase}}_screen.dart
@@ -1,7 +1,27 @@
+import 'package:clean_simple_eats/infrastructure/injection/injection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../application/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart';
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Screen extends StatelessWidget {
   const {{#pascalCase}}{{name}}{{/pascalCase}}Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<{{#pascalCase}}{{name}}{{/pascalCase}}Cubit>()..init(),
+        ),
+      ],
+      child: const _{{#pascalCase}}{{name}}{{/pascalCase}}Screen(),
+    );
+  }
+}
+
+class _{{#pascalCase}}{{name}}{{/pascalCase}}Screen extends StatelessWidget {
+  const _{{#pascalCase}}{{name}}{{/pascalCase}}Screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
