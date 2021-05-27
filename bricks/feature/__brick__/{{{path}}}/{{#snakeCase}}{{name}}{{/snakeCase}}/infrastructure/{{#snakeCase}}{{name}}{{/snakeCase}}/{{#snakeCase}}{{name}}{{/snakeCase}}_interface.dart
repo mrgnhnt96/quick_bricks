8a5fb485db1170ad76a15696f81836fb160d72c3
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-05-27 17:02:41 -0500

    made repo parts instead of imports

--- bricks/feature/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_interface.dart
+++ bricks/feature/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_interface.dart
@@ -1,1 +1,3 @@
+part of '{{#snakeCase}}{{name}}{{/snakeCase}}_entity.dao.dart';
+
 abstract class I{{#pascalCase}}{{name}}{{/pascalCase}}Repo {}
--- bricks/feature/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_source.dao.dart
+++ bricks/feature/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_source.dao.dart
@@ -1,8 +1,5 @@
-import 'package:injectable/injectable.dart';
-import 'package:clean_simple_eats/infrastructure/firebase/i_firebase.dart';
-
-import './{{#snakeCase}}{{name}}{{/snakeCase}}_interface.dart';
+part of '{{#snakeCase}}{{name}}{{/snakeCase}}_entity.dao.dart';
 
 @named
 @Injectable(as: I{{#pascalCase}}{{name}}{{/pascalCase}}Repo)
-class {{#pascalCase}}{{name}}{{/pascalCase}}Source with IFirebase implements I{{#pascalCase}}{{name}}{{/pascalCase}}Repo {}
+class {{#pascalCase}}{{name}}{{/pascalCase}}Source implements I{{#pascalCase}}{{name}}{{/pascalCase}}Repo {}
