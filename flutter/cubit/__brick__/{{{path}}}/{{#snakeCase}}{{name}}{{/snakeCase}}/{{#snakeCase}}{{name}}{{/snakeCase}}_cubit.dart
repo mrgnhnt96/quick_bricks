bf36aea2fb6a00483fc6268388c3f3febc362a37
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-06-24 17:53:15 -0500

    changed to is_hydrated. Added CacheCubit instead of hydrated cubit

--- flutter/cubit/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart
+++ flutter/cubit/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart
@@ -1,28 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-{{#isHydrated}}
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:injectable/injectable.dart';
-{{/isHydrated}}
+{{#is_hydrated}}
+import 'package:clean_simple_eats/features/cache_keys/domain/cache_cubit/cache_cubit.i.dart';
+{{/is_hydrated}}
+
 
 part '{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart';
 part '{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.freezed.dart';
-{{#isHydrated}}
+{{#is_hydrated}}
 part '{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.g.dart';
-{{/isHydrated}}
+{{/is_hydrated}}
 
-{{#isHydrated}}
 @injectable
-{{/isHydrated}}
-class {{#pascalCase}}{{name}}{{/pascalCase}}Cubit extends Cubit<{{#pascalCase}}{{name}}{{/pascalCase}}State> {{#isHydrated}} with HydratedMixin {{/isHydrated}}{
-  {{#pascalCase}}{{name}}{{/pascalCase}}Cubit() : super(const {{#pascalCase}}{{name}}{{/pascalCase}}State.ready());
+class {{#pascalCase}}{{name}}{{/pascalCase}}Cubit extends {{#is_hydrated}}Cache{{/is_hydrated}}Cubit<{{#pascalCase}}{{name}}{{/pascalCase}}State>{
+  {{#pascalCase}}{{name}}{{/pascalCase}}Cubit() : super(const {{#pascalCase}}{{name}}{{/pascalCase}}State.loading());
 
-  {{#isHydrated}}
+  void init() async {}
+
+
+  {{#is_hydrated}}
   @override
   {{#pascalCase}}{{name}}{{/pascalCase}}State? fromJson(Map<String, dynamic> json) =>
       {{#pascalCase}}{{name}}{{/pascalCase}}State.fromJson(json);
 
   @override
   Map<String, dynamic>? toJson({{#pascalCase}}{{name}}{{/pascalCase}}State state) => state.toJson();
-  {{/isHydrated}}
+  {{/is_hydrated}}
 }
