50903184533c47c442ec3d48371f6a18cf9548f9
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-06-24 18:36:23 -0500

    added state to class constructor name

--- flutter/cubit/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart
+++ flutter/cubit/__brick__/{{{path}}}/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart
@@ -2,7 +2,7 @@ part of '{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart';
 
 @freezed
 class {{#pascalCase}}{{name}}{{/pascalCase}}State with _${{#pascalCase}}{{name}}{{/pascalCase}}State {
-  const {{#pascalCase}}{{name}}{{/pascalCase}}._();
+  const {{#pascalCase}}{{name}}{{/pascalCase}}State._();
 
   const factory {{#pascalCase}}{{name}}{{/pascalCase}}State.loading() = _Loading;
   const factory {{#pascalCase}}{{name}}{{/pascalCase}}State.ready() = _Ready;
