834b1fda74cc0ab7432ae5ecbfdcacb590f872bb
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-06-25 11:55:03 -0500

    used escape method for return type

--- flutter/repo/__brick__/{{{feature_path}}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_source.dao.dart
+++ flutter/repo/__brick__/{{{feature_path}}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_source.dao.dart
@@ -5,7 +5,7 @@ part of '{{#snakeCase}}{{name}}{{/snakeCase}}_entity.dao.dart';
 class {{#pascalCase}}{{name}}{{/pascalCase}}Source extends I{{#pascalCase}}{{interface_name}}{{/pascalCase}}Source {
   {{#methods}}
   @override
-  {{returnType}} {{#camelCase}}{{name}}{{/camelCase}}({{#params}}{{start}}{{#isRequired}}required {{/isRequired}}{{type}} {{#camelCase}}{{name}}{{/camelCase}}{{#defaultValue}} = {{defaultValue}}{{/defaultValue}}, {{end}}{{/params}}) async {
+  {{{returnType}}} {{#camelCase}}{{name}}{{/camelCase}}({{#params}}{{start}}{{#isRequired}}required {{/isRequired}}{{type}} {{#camelCase}}{{name}}{{/camelCase}}{{#defaultValue}} = {{defaultValue}}{{/defaultValue}}, {{end}}{{/params}}) async {
     //TODO: implement {{#camelCase}}{{name}}{{/camelCase}}
     throw UnimplementedError('{{#camelCase}}{{name}}{{/camelCase}} has not been implemented!');
   }
