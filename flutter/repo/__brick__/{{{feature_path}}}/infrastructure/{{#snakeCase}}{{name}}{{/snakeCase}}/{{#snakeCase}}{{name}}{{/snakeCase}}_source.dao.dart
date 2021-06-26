7ebb8bad00b29f6491f791da890334e069b1d869
mrgnhnt96 <mrgnhnt96@gmail.com>
2021-06-26 18:14:41 -0500

    added IFirebase

--- flutter/repo/__brick__/{{{feature_path}}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_source.dao.dart
+++ flutter/repo/__brick__/{{{feature_path}}}/infrastructure/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}{{/snakeCase}}_source.dao.dart
@@ -2,7 +2,7 @@ part of '{{#snakeCase}}{{name}}{{/snakeCase}}_entity.dao.dart';
 
 @named
 @Injectable(as: I{{#pascalCase}}{{interface_name}}{{/pascalCase}}Source)
-class {{#pascalCase}}{{name}}{{/pascalCase}}Source extends I{{#pascalCase}}{{interface_name}}{{/pascalCase}}Source {
+class {{#pascalCase}}{{name}}{{/pascalCase}}Source extends I{{#pascalCase}}{{interface_name}}{{/pascalCase}}Source with IFirebase {
   {{#methods}}
   @override
   {{{returnType}}} {{#camelCase}}{{name}}{{/camelCase}}({{#params}}{{start}}{{#isRequired}}required {{/isRequired}}{{{type}}} {{#camelCase}}{{name}}{{/camelCase}}{{#defaultValue}} = {{{defaultValue}}}{{/defaultValue}}, {{end}}{{/params}}) async {
